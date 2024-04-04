37fac8875a12ee8aea2b0d2ee86aa3b797ad10b8
KyleMorrison99 <106417908+KyleMorrison99@users.noreply.github.com>
2024-04-04 11:41:40 +1100

    screening

--- literature_search/search_deduplication.R
+++ literature_search/search_deduplication.R
@@ -46,4 +46,22 @@ for (i in seq_along(chunks)) {
   
 }
 
+dat3 <-  read.csv(here("literature_screen", "full_text_screening", "total_full_text_screening.csv"), skip = 0)
 
+
+total_rows1 <- nrow(dat3)
+num_chunks1 <- ceiling(total_rows1 / 35) # Use ceiling to ensure all data is included
+
+# Create a sequence that assigns each row to a chunk
+chunk_assignment1 <- rep(1:num_chunks1, each=35, length.out=total_rows1)
+
+# Split the dataset into chunks based on the chunk assignments
+chunks1 <- split(dat3, chunk_assignment1)
+
+# Write function
+for (i in seq_along(chunks1)) {
+  # Construct file path
+  file_path <- sprintf("literature_screen/chunk_%d.csv", i)
+  # Use write.csv to save each chunk
+  write.csv(chunks1[[i]], file_path)
+}
